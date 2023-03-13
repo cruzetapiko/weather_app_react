877a173958c68cf6098f77c33902202446e4d059
cruzetapiko <simonetvia@outlook.com>
2023-03-13 16:56:30 -0600

    search component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import react from 'react';
+import Search from './Components/search/Search';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Search />
     </div>
   );
 }
