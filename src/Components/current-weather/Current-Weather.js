b2b8b7ebb39bd68071e36e7bbb68a0a4d8f57ee2
cruzetapiko <simonetvia@outlook.com>
2023-03-14 22:13:42 -0600

    weather static widget completed

--- src/Components/current-weather/Current-Weather.js
+++ src/Components/current-weather/Current-Weather.js
@@ -9,6 +9,30 @@ const CurrentWeather = () => {
         </div>
         <img alt='icon' className='icon' src='icons/01d.png' />
       </div>
+      <div className="bottom">
+        <p className='temperature'> 12°C </p>
+        <div className='details' >
+          <div className='parameters-row' >
+            <span className=' '>details</span>
+          </div>
+          <div className='parameters-row' >
+            <span className='parameter-label'>feels like </span>
+            <span className='parameter-value'> 7°C </span>
+          </div>
+          <div className='parameters-row' >
+            <span className='parameter-label'>Wind </span>
+            <span className='parameter-value'> 15 kl/h </span>
+          </div>
+          <div className='parameters-row' >
+            <span className='parameter-label'>humidity </span>
+            <span className='parameter-value'> 70% </span>
+          </div>
+          <div className='parameters-row' >
+            <span className='parameter-label'>pressure</span>
+            <span className='parameter-value'> 30 hpa </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
